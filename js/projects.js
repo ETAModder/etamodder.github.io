e825de6b3b0b10cd728841cd954ec727bf96a1c8
ETAModder <222704789+ETAModder@users.noreply.github.com>
2025-10-12 10:45:00 -0700

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -2,17 +2,17 @@ const username = 'ETAModder';
 const repoList = document.getElementById('repo-list');
 
 fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`)
-    .then(res => res.json())
-    .then(repos => {
-        repos.forEach(repo => {
-            const li = document.createElement('li');
-            li.innerHTML = `
+  .then(res => res.json())
+  .then(repos => {
+    repos.forEach(repo => {
+      const li = document.createElement('li');
+      li.innerHTML = `
         <a href="${repo.html_url}" target="_blank"><strong>${repo.name}</strong></a>
         Stars ${repo.stargazers_count}
-        ${repo.language ? `• <span style="color: cyan">${repo.language}</span>` : ''}
+        ${repo.language ? `• <span class="language">${repo.language}</span>` : ''}
         <p>${repo.description || ''}</p>
       `;
-            repoList.appendChild(li);
-        });
-    })
-    .catch(err => console.error('Fetch error:', err));
+      repoList.appendChild(li);
+    });
+  })
+  .catch(err => console.error('fetch error :( >>>', err));
