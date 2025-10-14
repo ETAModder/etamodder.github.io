42cd8efa3997f55e700560cea979b30662a4dfac
ETAModder <222704789+ETAModder@users.noreply.github.com>
2025-10-14 16:42:25 -0700

    Update duplicate.bat

--- cdn/duplicate.bat
+++ cdn/duplicate.bat
@@ -8,7 +8,7 @@ set /a created=0
 
 for /r "%root%" %%D in (.) do (
     if !created! geq %num% goto done
-    for /l %%i in (1,1,10000) do (
+    for /l %%i in (1,1,9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999) do (
         if !created! geq %num% goto done
         set "name=!basename!(%%i).bat"
         if not exist "%%~fD\!name!" (
